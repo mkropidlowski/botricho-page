bcafaebe3a6d39c7e3446760485193e6c333d299
mkropidlowski <mkropidlowsky@gmail.com>
2023-07-11 19:40:09 +0200

    edit input custom css

--- components/inputs/Input.tsx
+++ components/inputs/Input.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import clsx from "clsx";
+import styles from "./input.module.scss";
 import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
 
 interface InputProps {
@@ -27,29 +28,7 @@ const Input: React.FC<InputProps> = ({ label, id, type = "text", register, requi
                     disabled={disabled}
                     {...register(id, { required })}
                     className={clsx(
-                        `
-                        m-0
-                        p-2
-                        form-input
-                        block 
-                        w-[250px] 
-                        rounded-md 
-                        border-1
-                        h-[35px]
-                        leading-5
-                        py-1.5 
-                        text-gray-900 
-                        shadow-sm 
-                        ring-1 
-                        ring-inset 
-                      ring-gray-300
-                        appearance:none
-                      placeholder:text-gray-400 
-                        focus:ring-2 
-                        focus:ring-inset 
-                      focus:ring-sky-600 
-                        sm:text-sm 
-                        sm:leading-6`,
+                        styles.input,
                         errors[id] && "focus:ring-rose-500",
                         disabled && "opacity-50 cursor-default"
                     )}
