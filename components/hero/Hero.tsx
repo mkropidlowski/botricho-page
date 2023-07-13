910d9175f60c7c376798da69bf74039e0ffbc8e0
mkropidlowski <mkropidlowsky@gmail.com>
2023-07-13 22:22:40 +0200

    changes in hero & navbar

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -9,18 +9,22 @@ interface HeroProps {
 
 const Hero: FC<HeroProps> = ({ className }) => {
     return (
-        <div className={clsx("flex justify-center", className)}>
+        <div className={clsx("md:justify-center justify-start", className)}>
             <HeroImage />
-            <div className="flex flex-col gap-5 relative">
+            <div className="flex flex-col gap-5 relative top-[150px] md:top-0 p-5">
                 <div>
-                    <h1 className="uppercase md:text-[37px] text-[30px] font-medium">Gabinet Botricho</h1>
-                    <p className="p-4 md:text-4xl text-3xl max-w-3xl">
+                    <h1 className="uppercase md:text-[37px] text-[27px] font-semibold">Gabinet Botricho</h1>
+                    <p className="p-4 md:text-4xl text-[23px] max-w-3xl font-normal">
                         Specjalizujemy się w trychologi praktycznej i kosmetologi.
                     </p>
                 </div>
-                <Link href="https://botricho.booksy.com" className="flex justify-center cursor-pointer z-[150]">
-                    <button className="md:h-[40px] h-[55px] rounded-lg bg-green-600 font-medium text-white pl-3 pr-3 cursor-pointer">
-                        Zarezeruj wizytę za pomocą Booksy
+                <Link
+                    href="https://botricho.booksy.com"
+                    target="_blank"
+                    className="cursor-pointer z-[30] max-w-[360px]"
+                >
+                    <button className=" md:h-[50px] h-[60px] text-lg rounded-lg bg-green-600 font-medium text-white pl-3 pr-3 cursor-pointer">
+                        Zarezeruj wizytę za pomocą Booksy!
                     </button>
                 </Link>
             </div>
